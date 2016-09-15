88bbd94d10d3d53f459e1e05868dc368347c37c9
yangyunxin <850801584@qq.com>
2016-09-15 08:16:39 +0800

    add two entry

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,20 +3,25 @@ var webpack = require('webpack');
 console.log('------------kaishi---------')
 module.exports = {
 	devtoll: 'cheap-module-eval-source-map',
-	entry: [
-		// necessary for hot reloading with IE:
-		'eventsource-polyfill',
-		// listen to code updates emitted by hot middleware:
-		'webpack-hot-middleware/client',
-		// your code:
-		'./src/index'
-	],
+	// entry: [
+	// 	// necessary for hot reloading with IE:
+	// 	'eventsource-polyfill',
+	// 	// listen to code updates emitted by hot middleware:
+	// 	'webpack-hot-middleware/client',
+	// 	// your code:
+	// 	'./src/index'
+	// ],
+	entry: {
+		app: ['eventsource-polyfill','webpack-hot-middleware/client','./src/index'],
+		vendor: ['react']
+	},
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
+		filename: '[name].js',
 		publicPath: '/dist/'
 	},
 	plugins: [
+		new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js', Infinity),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin()
 	],
